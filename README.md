fd409a87d2ba12607e18d3c0ba0f861fa2880168
jensschuppe <schuppe@systopia.de>
2023-03-13 12:35:48 +0100

    Extension skeleton

---
+++ README.md
@@ -0,0 +1,10 @@
+# Configuration Profiles
+
+This extension provides a data structure for configuration that is organized in
+configurable profiles, representing instances of sets of configuration options
+for multi-purpose use.
+
+This extension is most likely only needed to be installed as a dependency of
+other extensions.
+
+The extension is licensed under [AGPL-3.0](LICENSE.txt).
---
+++ config_profiles.php
@@ -0,0 +1,61 @@
+<?php
+
+require_once 'config_profiles.civix.php';
+// phpcs:disable
+use CRM_ConfigProfiles_ExtensionUtil as E;
+// phpcs:enable
+
+/**
+ * Implements hook_civicrm_config().
+ *
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_config/
+ */
+function config_profiles_civicrm_config(&$config): void {
+  _config_profiles_civix_civicrm_config($config);
+}
+
+/**
+ * Implements hook_civicrm_install().
+ *
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_install
+ */
+function config_profiles_civicrm_install(): void {
+  _config_profiles_civix_civicrm_install();
+}
+
+/**
+ * Implements hook_civicrm_enable().
+ *
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_enable
+ */
+function config_profiles_civicrm_enable(): void {
+  _config_profiles_civix_civicrm_enable();
+}
+
+// --- Functions below this ship commented out. Uncomment as required. ---
+
+/**
+ * Implements hook_civicrm_preProcess().
+ *
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_preProcess
+ */
+//function config_profiles_civicrm_preProcess($formName, &$form): void {
+//
+//}
+
+/**
+ * Implements hook_civicrm_navigationMenu().
+ *
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_navigationMenu
+ */
+//function config_profiles_civicrm_navigationMenu(&$menu): void {
+//  _config_profiles_civix_insert_navigation_menu($menu, 'Mailings', [
+//    'label' => E::ts('New subliminal message'),
+//    'name' => 'mailing_subliminal_message',
+//    'url' => 'civicrm/mailing/subliminal',
+//    'permission' => 'access CiviMail',
+//    'operator' => 'OR',
+//    'separator' => 0,
+//  ]);
+//  _config_profiles_civix_navigationMenu($menu);
+//}
