cbcb73c3bd534e357a9ca51c05e4a67436e8314f
Jens Schuppe <schuppe@systopia.de>
2023-05-19 17:04:00 +0200

    Move afform-related event listeners to the BAO class

--- CRM/ConfigProfiles/BAO/ConfigProfile.php
+++ CRM/ConfigProfiles/BAO/ConfigProfile.php
@@ -18,7 +18,8 @@ class CRM_ConfigProfiles_BAO_ConfigProfile extends CRM_ConfigProfiles_DAO_Config
    */
   public static function getSubscribedEvents(): array {
     return [
-      'civi.afform_admin.metadata' => 'afformAdminMetadata',
+      'civi.afform_admin.metadata' => 'afformEntityTypes',
+      'civi.afform.get' => 'getConfigProfileAfforms',
     ];
   }
 
@@ -77,19 +78,107 @@ class CRM_ConfigProfiles_BAO_ConfigProfile extends CRM_ConfigProfiles_DAO_Config
   }
 
   /**
-   * Provides Afform metadata about this entity.
+   * Make configuration profiles available to FormBuilder.
    *
-   * @see \Civi\AfformAdmin\AfformAdminMeta::getMetadata().
+   * @param GenericHookEvent $e
    */
-  public static function afformAdminMetadata(GenericHookEvent $event): void {
-    $entity = 'ConfigProfile';
-    $event->entities[$entity] = [
+  public static function afformEntityTypes(GenericHookEvent $e) {
+    $event->entities['ConfigProfile'] = [
       'entity' => $entity,
-      'label' => E::ts('Configuration Profile'),
+      'label' => E::ts('Configuration Profile (generic)'),
       'icon' => 'fa-cogs',
       'type' => 'primary',
       'defaults' => '{}',
     ];
+
+    foreach (\CRM_ConfigProfiles_BAO_ConfigProfile::getTypes() as $profile_type) {
+      $e->entities[$profile_type['entity_name']] = [
+        'entity' => $profile_type['entity_name'],
+        'label' => E::ts('%1 Configuration Profile', [1 => $profile_type['label']]),
+        'icon' => $profile_type['icon'],
+        'type' => 'primary',
+        'defaults' => '{}',
+      ];
+    }
+  }
+
+  /**
+   * Generates Afform forms for each configuration profile type.
+   *
+   * @param \Civi\Core\Event\GenericHookEvent $event
+   * @throws \API_Exception
+   */
+  public static function getConfigProfileAfforms($event) {
+    // Early return if forms are not requested.
+    if ($event->getTypes && !in_array('form', $event->getTypes, TRUE)) {
+      return;
+    }
+
+    $afforms =& $event->afforms;
+    $getNames = $event->getNames;
+
+    // Early return if this API call is fetching afforms by name and those names are not related to ConfigProfiles.
+    if (
+      (!empty($getNames['name']) && !strstr(implode(' ', $getNames['name']), 'ConfigProfile_'))
+      || (!empty($getNames['module_name']) && !strstr(implode(' ', $getNames['module_name']), 'ConfigProfile'))
+      || (!empty($getNames['directive_name']) && !strstr(implode(' ', $getNames['directive_name']), 'config_profile'))
+    ) {
+      return;
+    }
+
+    foreach (\CRM_ConfigProfiles_BAO_ConfigProfile::getTypes() as $profile_type) {
+      // Submission form to create/edit profiles of each profile type.
+      $name = 'afform' . $profile_type['entity_name'];
+      $item = [
+        'name' => $name,
+        'type' => 'form',
+        'title' => E::ts('%1 Configuration Profile', [1 => $profile_type['label']]),
+        'description' => '',
+        'base_module' => E::LONG_NAME,
+        'is_dashlet' => FALSE,
+        'is_public' => FALSE,
+        'is_token' => FALSE,
+        'permission' => 'administer CiviCRM',
+        'server_route' => "civicrm/admin/config-profile/{$profile_type['name']}/edit",
+      ];
+      if ($event->getLayout) {
+        $fields = \Civi\Api4\ConfigProfile::getFields()
+          ->addValue('type', $profile_type['name'])
+          ->addSelect('name')
+          ->addWhere('readonly', 'IS EMPTY')
+          ->addWhere('input_type', 'IS NOT EMPTY')
+          // Don't allow type to be changed on the form, since this form is specific to type.
+          ->addWhere('name', '!=', 'type')
+          // Don't include the serialized "data" field in the form, as individual data properties will have their own fields per type.
+          ->addWhere('name', '!=', 'data')
+          ->execute();
+        $item['layout'] = \CRM_Core_Smarty::singleton()->fetchWith('ang/afformConfigProfile.tpl', [
+          'profileType' => $profile_type,
+          'fields' => $fields,
+        ]);
+      }
+      $afforms[$name] = $item;
+
+      // Search listing for for each profile type.
+      $name = 'afsearch' . $profile_type['entity_name'] . '_listing';
+      $item = [
+        'name' => $name,
+        'type' => 'search',
+        'title' => E::ts('%1 Configuration Profile', [1 => $profile_type['label']]),
+        'description' => E::ts('Search listing for %1 configuration profiles', [1 => $profile_type['label']]),
+        'base_module' => E::LONG_NAME,
+        'is_dashlet' => FALSE,
+        'is_public' => FALSE,
+        'is_token' => FALSE,
+        'permission' => 'administer CiviCRM',
+        'server_route' => "civicrm/admin/config-profile/{$profile_type['name']}",
+        'requires' => ['crmSearchDisplayTable'],
+      ];
+      $item['layout'] = \CRM_Core_Smarty::singleton()->fetchWith('ang/afsearch_config-profiles_listing.tpl', [
+        'profileType' => $profile_type,
+      ]);
+      $afforms[$name] = $item;
+    }
   }
 
 }
--- Civi/ConfigProfiles/Api/ConfigProfile.php
+++ Civi/ConfigProfiles/Api/ConfigProfile.php
@@ -16,8 +16,6 @@ class ConfigProfile implements EventSubscriberInterface, ApiProviderInterface {
   public static function getSubscribedEvents() {
     return [
       'civi.api4.entityTypes' => ['onApi4EntityTypes', Events::W_EARLY],
-      'civi.afform_admin.metadata' => 'afformEntityTypes',
-      'civi.afform.get' => 'getConfigProfileAfforms',
     ];
   }
 
@@ -81,102 +79,6 @@ class ConfigProfile implements EventSubscriberInterface, ApiProviderInterface {
       'class' => '\Civi\Api4\ConfigProfile',
       'icon' => 'fa-cogs',
     ];
-  }
-
-  /**
-   * Make configuration profiles available to FormBuilder.
-   *
-   * @param GenericHookEvent $e
-   */
-  public static function afformEntityTypes(GenericHookEvent $e) {
-    foreach (\CRM_ConfigProfiles_BAO_ConfigProfile::getTypes() as $profile_type) {
-      $e->entities[$profile_type['entity_name']] = [
-        'entity' => $profile_type['entity_name'],
-        'label' => E::ts('%1 Configuration Profile', [1 => $profile_type['label']]),
-        'icon' => $profile_type['icon'],
-        'type' => 'primary',
-        'defaults' => '{}',
-      ];
-    }
-  }
-
-  /**
-   * Generates Afform forms for each configuration profile type.
-   *
-   * @param \Civi\Core\Event\GenericHookEvent $event
-   * @throws \API_Exception
-   */
-  public static function getConfigProfileAfforms($event) {
-    // Early return if forms are not requested.
-    if ($event->getTypes && !in_array('form', $event->getTypes, TRUE)) {
-      return;
-    }
-
-    $afforms =& $event->afforms;
-    $getNames = $event->getNames;
-
-    // Early return if this API call is fetching afforms by name and those names are not related to ConfigProfiles.
-    if (
-      (!empty($getNames['name']) && !strstr(implode(' ', $getNames['name']), 'ConfigProfile_'))
-      || (!empty($getNames['module_name']) && !strstr(implode(' ', $getNames['module_name']), 'ConfigProfile'))
-      || (!empty($getNames['directive_name']) && !strstr(implode(' ', $getNames['directive_name']), 'config_profile'))
-    ) {
-      return;
-    }
-
-    foreach (\CRM_ConfigProfiles_BAO_ConfigProfile::getTypes() as $profile_type) {
-      // Submission form to create/edit profiles of each profile type.
-      $name = 'afform' . $profile_type['entity_name'];
-      $item = [
-        'name' => $name,
-        'type' => 'form',
-        'title' => E::ts('%1 Configuration Profile', [1 => $profile_type['label']]),
-        'description' => '',
-        'base_module' => E::LONG_NAME,
-        'is_dashlet' => FALSE,
-        'is_public' => FALSE,
-        'is_token' => FALSE,
-        'permission' => 'administer CiviCRM',
-        'server_route' => "civicrm/admin/config-profile/{$profile_type['name']}/edit",
-      ];
-      if ($event->getLayout) {
-        $fields = \Civi\Api4\ConfigProfile::getFields()
-          ->addValue('type', $profile_type['name'])
-          ->addSelect('name')
-          ->addWhere('readonly', 'IS EMPTY')
-          ->addWhere('input_type', 'IS NOT EMPTY')
-          // Don't allow type to be changed on the form, since this form is specific to type.
-          ->addWhere('name', '!=', 'type')
-          // Don't include the serialized "data" field in the form, as individual data properties will have their own fields per type.
-          ->addWhere('name', '!=', 'data')
-          ->execute();
-        $item['layout'] = \CRM_Core_Smarty::singleton()->fetchWith('ang/afformConfigProfile.tpl', [
-          'profileType' => $profile_type,
-          'fields' => $fields,
-        ]);
-      }
-      $afforms[$name] = $item;
-
-      // Search listing for for each profile type.
-      $name = 'afsearch' . $profile_type['entity_name'] . '_listing';
-      $item = [
-        'name' => $name,
-        'type' => 'search',
-        'title' => E::ts('%1 Configuration Profile', [1 => $profile_type['label']]),
-        'description' => E::ts('Search listing for %1 configuration profiles', [1 => $profile_type['label']]),
-        'base_module' => E::LONG_NAME,
-        'is_dashlet' => FALSE,
-        'is_public' => FALSE,
-        'is_token' => FALSE,
-        'permission' => 'administer CiviCRM',
-        'server_route' => "civicrm/admin/config-profile/{$profile_type['name']}",
-        'requires' => ['crmSearchDisplayTable'],
-      ];
-      $item['layout'] = \CRM_Core_Smarty::singleton()->fetchWith('ang/afsearch_config-profiles_listing.tpl', [
-        'profileType' => $profile_type,
-      ]);
-      $afforms[$name] = $item;
-    }
   }
 
   /**
