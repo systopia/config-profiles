a08331c0337e2e483a381e911878671953de9121
Jens Schuppe <schuppe@systopia.de>
2023-11-29 14:53:12 +0100

    Derive type from ID when saving

--- Civi/ConfigProfiles/Api4/Action/SaveTrait.php
+++ Civi/ConfigProfiles/Api4/Action/SaveTrait.php
@@ -2,6 +2,8 @@
 
 namespace Civi\ConfigProfiles\Api4\Action;
 
+use Civi\Api4\ConfigProfile;
+
 trait SaveTrait {
 
   /**
@@ -16,17 +18,33 @@ trait SaveTrait {
   protected function write(array $items) {
     $types = \CRM_ConfigProfiles_BAO_ConfigProfile::getTypes();
     foreach ($items as &$item) {
-      $type = $item['type'];
+      $type = $item['type'] ?? self::getType($item['id'] ?? NULL);
+      if (!isset($type)) {
+        throw new \Exception('Unable to determine ConfigProfile type.');
+      }
+
       /* @var \Civi\ConfigProfiles\ConfigProfileInterface $class */
       $class = $types[$type]['class'];
       foreach (array_keys($class::getFields()) as $field_name) {
-        $item['data'][$field_name] = $item[$field_name];
+        if (isset($item[$field_name])) {
+          $item['data'][$field_name] = $item[$field_name];
+        }
         unset($item[$field_name]);
       }
       $class::processValues($item);
     }
 
     return \CRM_ConfigProfiles_BAO_ConfigProfile::writeRecords($items);
+  }
+
+  public static function getType(int $id = NULL): ?string {
+    return isset($id)
+      ? ConfigProfile::get(NULL, FALSE)
+        ->addSelect('type')
+        ->addWhere('id', '=', $id)
+        ->execute()
+        ->single()['type']
+      : NULL;
   }
 
 }
